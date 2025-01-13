70ca37755093a88666c831f73080df69d3b263bb
Andriy2444 <andriy241179@gmail.com>
2025-01-13 20:25:36 +0200

    Initial commit

--- app.py
+++ app.py
@@ -1,10 +1,52 @@
 from flask import Flask
+import mysql.connector
+import os
 
 app = Flask(__name__)
 
+# Отримуємо змінні середовища
+db_host = os.getenv('DB_HOST')
+db_port = os.getenv('DB_PORT', 3306)
+db_name = os.getenv('DB_NAME')
+db_user = os.getenv('DB_USER')
+db_password = os.getenv('DB_PASSWORD')
+
+
+# Підключення до бази даних MySQL
+def get_db_connection():
+    connection = mysql.connector.connect(
+        host=db_host,
+        port=db_port,
+        database=db_name,
+        user=db_user,
+        password=db_password
+    )
+    return connection
+
+
 @app.route("/")
 def home():
-    return "Hello, Render!"
+    try:
+        # Підключення до бази
+        connection = get_db_connection()
+        cursor = connection.cursor()
+
+        # Приклад запиту: отримуємо всі користувачів
+        cursor.execute("SELECT * FROM users;")
+        users = cursor.fetchall()
+
+        # Формуємо відповідь на основі даних з бази
+        users_list = "\n".join([f"Name: {user[1]}, Password: {user[2]}" for user in users])
+
+        return f"Hello, Render!\nUsers in DB:\n{users_list}"
+
+    except mysql.connector.Error as err:
+        return f"Помилка підключення до бази даних: {err}"
+
+    finally:
+        if 'connection' in locals() and connection.is_connected():
+            connection.close()
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
