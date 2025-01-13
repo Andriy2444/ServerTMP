c8d60dc74d5ed5221c02c6f2a9052bc7c686515a
Andriy2444 <andriy241179@gmail.com>
2025-01-13 20:58:25 +0200

    add

--- app.py
+++ app.py
@@ -6,11 +6,14 @@ app = Flask(__name__)
 
 # Отримуємо змінні середовища
 db_host = os.getenv('DB_HOST')
-db_port = os.getenv('DB_PORT', 16912)
+db_port = os.getenv('DB_PORT')
 db_name = os.getenv('DB_NAME')
 db_user = os.getenv('DB_USER')
 db_password = os.getenv('DB_PASSWORD')
 
+# Перевірка на наявність всіх змінних середовища
+if not all([db_host, db_port, db_name, db_user, db_password]):
+    raise ValueError("Не всі змінні середовища для бази даних задані!")
 
 # Підключення до бази даних MySQL
 def get_db_connection():
@@ -23,30 +26,23 @@ def get_db_connection():
     )
     return connection
 
-
 @app.route("/")
 def home():
     try:
         # Підключення до бази
-        connection = get_db_connection()
-        cursor = connection.cursor()
+        with get_db_connection() as connection:
+            with connection.cursor() as cursor:
+                # Приклад запиту: отримуємо всі користувачів
+                cursor.execute("SELECT * FROM users;")
+                users = cursor.fetchall()
 
-        # Приклад запиту: отримуємо всі користувачів
-        cursor.execute("SELECT * FROM users;")
-        users = cursor.fetchall()
-
-        # Формуємо відповідь на основі даних з бази
-        users_list = "\n".join([f"Name: {user[1]}, Password: {user[2]}" for user in users])
+                # Формуємо відповідь на основі даних з бази
+                users_list = "\n".join([f"Name: {user[1]}, Password: {user[2]}" for user in users])
 
         return f"Hello, Render!\nUsers in DB:\n{users_list}"
 
     except mysql.connector.Error as err:
         return f"Помилка підключення до бази даних: {err}"
-
-    finally:
-        if 'connection' in locals() and connection.is_connected():
-            connection.close()
-
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
